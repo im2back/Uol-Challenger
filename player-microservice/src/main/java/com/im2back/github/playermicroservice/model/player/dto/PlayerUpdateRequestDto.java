404c4c23d11785a55edc981983a75fd0bb9c40f7
Jefferson Richards <jefferson.richards@outlook.com>
2024-05-03 10:21:36 -0300

    feat: Player Microservice - Bean validation(DTO'S and Endpoints)

--- player-microservice/src/main/java/com/im2back/github/playermicroservice/model/player/dto/PlayerUpdateRequestDto.java
+++ player-microservice/src/main/java/com/im2back/github/playermicroservice/model/player/dto/PlayerUpdateRequestDto.java
@@ -1,5 +1,9 @@
 package com.im2back.github.playermicroservice.model.player.dto;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import com.im2back.github.playermicroservice.model.group.Group;
 
 import lombok.AllArgsConstructor;
@@ -15,12 +19,17 @@ import lombok.Setter;
 @EqualsAndHashCode
 public class PlayerUpdateRequestDto {
 	
+	@NotNull(message = "O ID não pode estar em branco")
 	private Long id;
 	
+	@NotBlank(message = "O nome não pode estar em branco")
+	@Size(min = 6, max = 20, message = "O nome de usuário deve ter entre 5 e 20 caracteres.")
 	private String name;
 
+	@NotBlank(message = "O e-mail não pode estar em branco")
 	private String email;
 
+	@Size(min = 11, max = 11, message = "O campo phone deve conter 11 caracteres.")
 	private String phone;
 
 	private String alias;
