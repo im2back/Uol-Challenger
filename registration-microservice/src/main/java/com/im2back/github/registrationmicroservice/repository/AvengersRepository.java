76731b3ba330ba0bfe622339b00c3bc49f4b4f97
Jefferson Richards <jefferson.richards@outlook.com>
2024-05-13 11:41:06 -0300

    feat : adjustments

--- registration-microservice/src/main/java/com/im2back/github/registrationmicroservice/repository/AvengersRepository.java
+++
@@ -1,9 +0,0 @@
-package com.im2back.github.registrationmicroservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.im2back.github.registrationmicroservice.model.entities.vingadores.Avengers;
-
-public interface AvengersRepository extends JpaRepository<Avengers, Long> {
-
-}
--- registration-microservice/src/main/java/com/im2back/github/registrationmicroservice/repository/JusticeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.im2back.github.registrationmicroservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.im2back.github.registrationmicroservice.model.entities.ligadajustica.JusticeLeague;
-
-public interface JusticeRepository extends JpaRepository<JusticeLeague, Long> {
-
-}
